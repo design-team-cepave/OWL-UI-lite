86bd907690a96f2abba916fdf2082676fca3acfb
JJ <jerry7722@hotmail.com>
2016-10-08 13:24:19 +0800

    update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,50 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    pug         = require('gulp-pug'),
-    plumber     = require('gulp-plumber'),
-    watch       = require('gulp-watch'),
-    uglify      = require('gulp-uglify'),
-    cssnano     = require('gulp-cssnano'),
-    compass     = require('gulp-compass'),
-    imagemin    = require('gulp-imagemin'),
-    src         = './src';
-    dist        = './dist';
+var gulp          = require('gulp'),
+    $             = require('gulp-load-plugins')(),
+    browserSync   = require('browser-sync'),
+    argv          = require('yargs').argv;
+    browserReload = browserSync.reload,
+    src           = './src';
+    dist          = './dist';
 
-gulp.task('styles', function(){
+gulp.task('browser-sync', function() {
+  browserSync({
+    open: !!argv.open,
+    notify: !!argv.notify,
+    server: {
+      baseDir: './dist'
+    }
+  });
+});
+
+gulp.task('ui-styles', function(){
   gulp.src('sass/main.scss', {cwd: src})
-    .pipe(plumber())
-    .pipe(compass({
-      css: 'dist/css',
-      sass: 'src/sass'
+    .pipe($.plumber())
+    .pipe($.compass({
+      sass: 'src/sass',
+      css: 'dist/css'
     }))
     .pipe(gulp.dest('css', {cwd: dist}))
-    .pipe(browserSync.reload({stream: true}));
 });
+
+gulp.task('doc-styles', function(){
+  gulp.src('sass/master.scss', {cwd: src})
+    .pipe($.plumber())
+    .pipe($.compass({
+      sass: 'src/sass',
+      css: 'dist/assets/css'
+    }))
+    .pipe(gulp.dest('css', {cwd: 'dist/assets'}))
+});
+
+gulp.task('styles', ['ui-styles', 'doc-styles']);
 
 gulp.task('views', function buildHTML() {
   return gulp.src('pug/index.pug', {cwd: src})
-  .pipe(plumber())
-  .pipe(pug({
+  .pipe($.plumber())
+  .pipe($.pug({
     pretty: true
   }))
   .pipe(gulp.dest(dist))
-  .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('js-watch', function(){
@@ -37,33 +52,33 @@ gulp.task('js-watch', function(){
     .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: dist
-    }
-  });
-  gulp.watch('sass/**/*.{sass, scss}', {cwd: src}, ['styles']);
-  gulp.watch('pug/**/*.pug', {cwd: src}, ['views']);
-  gulp.watch('scripts/*.js', {cwd: dist}, ['js-watch']);
-  gulp.watch('*.html', {cwd: dist}).on('change', browserSync.reload);
+gulp.task('watch', function () {
+  gulp.watch('sass/main.scss', {cwd: src}, ['styles', browserReload]);
+});
+
+gulp.task('serve', ['build', 'browser-sync'],function() {
+  gulp.watch('sass/**/*.scss', {cwd: src}, ['styles', browserReload]);
+  gulp.watch('pug/**/*.pug', {cwd: src}, ['views', browserReload]);
+  gulp.watch('scripts/*.js', {cwd: dist}, ['js-watch', browserReload]);
+  gulp.watch('*.html', {cwd: dist}).on('change', browserReload);
+  browserSync.create();
 });
 
 gulp.task('image-min', function(){
   gulp.src('img/*', {cwd: dist})
-    .pipe(imagemin())
+    .pipe($.imagemin())
     .pipe(gulp.dest('img', {cwd: dist}));
 });
 
 gulp.task('js-min', function(){
   gulp.src('scripts/*.js', {cwd: dist})
-    .pipe(uglify())
+    .pipe($.uglify())
     .pipe(gulp.dest('scripts', {cwd: dist}));
 });
 
 gulp.task('css-min', function(){
   gulp.src('css/*.css', {cwd: dist})
-    .pipe(cssnano())
+    .pipe($.cssnano())
     .pipe(gulp.dest('css', {cwd: dist}));
 });
 
